8be776079fc9169b7c93e64c23da5c6e5a19ede5
ansaam <elaf_ef@hotmail.com>
2023-09-03 00:07:17 +0300

    تم حل الواجب 3

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # write your code here
+favorite_animals = ["dog ","cat","monkey","rabbit"]
+print(favorite_animals)
+print(f"the third element in the list is {favorite_animals[1]}")
+favorite_animals.remove("monkey")
+print(f"{favorite_animals}")
+favorite_animals.append("panda")
+print(f"{favorite_animal}")
+for favorite_animal in favorite_animal:
+    print(f'I love{favorite_animal}')
+
+
+    numbers=[5,4,3,2,1]
+    number_sum=0
+    for number in numbers:
+        number_sum=number+number_sum
+    print(number_sum)    
+
+
+
